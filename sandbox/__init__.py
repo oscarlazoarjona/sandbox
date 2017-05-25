2f307681a7b1f40af9cc0b04ef7cce9e979939fe
Oscar Gerardo Lazo Arjona <oscar.lazoarjona@physics.ox.ac.uk>
2017-05-25 14:54:22 +0100

    Added some functionality.

---
+++ sandbox/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2017 Oscar Gerardo Lazo Arjona
+# mailto: oscar.lazoarjona@physics.ox.ac.uk
+"""There is not enough sand in the world."""
+
+
+def castle():
+    """There is not enough sand in the world."""
+    print "There is not enough sand in the world."
